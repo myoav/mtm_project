2d00902f7cca465f1d97fa2852f0013923d89798
malul88 <malul88>
2020-08-03 18:43:59 +0300

    change edges

--- Edges.cpp
+++ Edges.cpp
@@ -3,3 +3,49 @@
 //
 
 #include "Edges.h"
+
+Edges::Edges(std::set<std::pair<std::string, std::string>> edges):edges(edges){}
+
+Edges::Edges() {}
+
+Edges::Edges(const Edges &edges) {
+    std::set<std::pair<std::string,std::string>> temp(edges.edges);
+    this->edges = temp;
+}
+
+Edges &Edges::operator=(const Edges &edges) {
+    if (this == &edges){
+        return *this;
+    }
+    this->edges.operator=(edges.edges);
+    return *this;
+}
+
+void Edges::addEdge(std::pair<std::string, std::string> edge) {
+    edges.insert(edge);
+}
+
+void Edges::deleteEdge(std::pair<std::string, std::string> edge) {
+    edges.erase(edge);
+}
+
+bool Edges::isContain(std::pair<std::string, std::string> edge) const{
+    std::set<std::pair<std::string,std::string>>::iterator it;
+    it = edges.find(edge);
+    return it != edges.end();
+}
+
+std::set<std::pair<std::string, std::string>>::iterator Edges::begin() const {
+    std::set<std::pair<std::string,std::string>>::iterator it = edges.begin();
+    return it;
+}
+
+std::set<std::pair<std::string, std::string>>::iterator Edges::end() const {
+    std::set<std::pair<std::string,std::string>>::iterator it = edges.end();
+    return it;
+}
+
+
+Edges operator+(const Edges &edges1, const Edges &edges2) {
+    return Edges();
+}
--- Edges.h
+++ Edges.h
@@ -1,13 +1,36 @@
 
 #ifndef GCALC_EDGES_H
 #define GCALC_EDGES_H
+#include <iostream>
+#include <utility>
+#include <set>
 
 
 class Edges {
 private:
+    std::set<std::pair<std::string,std::string>> edges;
+public:
+    Edges(std::set<std::pair<std::string,std::string>> edges);
 
+    Edges();
+
+    Edges( const Edges &edges);
+
+    Edges &operator=(const Edges &edges);
+
+    void addEdge(std::pair<std::string,std::string> edge);
+
+    void deleteEdge(std::pair<std::string,std::string> edge);
+
+    bool isContain(std::pair<std::string, std::string> edge) const ;
+
+    std::set<std::pair<std::string,std::string>>::iterator begin() const;
+
+    std::set<std::pair<std::string,std::string>>::iterator end() const;
 
 };
+
+Edges operator+(const Edges &edges1, const Edges &edges2);
 
 
 #endif //GCALC_EDGES_H
